7ca7f3a65225e86da52b61a63ec0e2e0e2e6c799
Evgeniya <eva.sharapova1@mail.ru>
2025-08-05 15:44:41 +0300

    V3_2

--- src/main/java/ru/yandex/practicum/filmorate/rowMapper/UserRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/rowMapper/UserRowMapper.java
@@ -5,7 +5,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.util.Optional;
 
 public class UserRowMapper implements RowMapper<User> {
     @Override
@@ -16,10 +15,6 @@ public class UserRowMapper implements RowMapper<User> {
         user.setLogin(resultSet.getObject("login", String.class));
         user.setName(resultSet.getObject("name", String.class));
         user.setBirthday(resultSet.getObject("birthday", LocalDate.class));
-        /*
-        if (Optional.ofNullable(resultSet.getObject("friend_id", Long.class)).isPresent()) {
-            user.addFriend(resultSet.getObject("friend_id", Long.class));
-        }*/
         return user;
     }
 }
